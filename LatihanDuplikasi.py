acdc15c2d7c7964105f7b3661a92a1871f237eb5
DeriRsd <113176962+DeriRsd@users.noreply.github.com>
2024-03-24 20:35:06 +0700

    Update LatihanDuplikasi.py

--- LatihanDuplikasi.py
+++ LatihanDuplikasi.py
@@ -12,3 +12,6 @@ print("  *****  "*2)
 
 #contoh 2
 print("         *\n       * *\n     *   *\n   *     *\n * * * * *\n"*2)
+
+#contoh 3
+print(" + * * * * +\n *         *\n *         *\n *         *\n + * * * * +\n"*2)
