f0f6bafa33aad6fd8effff5e3f8721fe12e1d7ff
DeriRsd <113176962+DeriRsd@users.noreply.github.com>
2024-03-24 20:27:25 +0700

    Latihan Python Machine Learning
    Latihan Duplikasi Python

---
+++ LatihanDuplikasi.py
@@ -0,0 +1,14 @@
+# (*2) Pada Akhiran agar menduplikasi
+
+#contoh 1
+print("    *    "*2)
+print("   * *   "*2)
+print("  *   *  "*2)
+print(" *     * "*2)
+print("***   ***"*2)
+print("  *   *  "*2)
+print("  *   *  "*2)
+print("  *****  "*2)
+
+#contoh 2
+print("         *\n       * *\n     *   *\n   *     *\n * * * * *\n"*2)
