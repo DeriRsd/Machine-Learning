6fc0db72b2069524f2124dd72c635f771a9a22b9
DeriRsd <113176962+DeriRsd@users.noreply.github.com>
2024-03-25 20:29:30 +0700

    Latihan Python Minggu 2
    Latihan Basic Python

---
+++ Minggu2/LatihanDuplikasi.py
@@ -0,0 +1,17 @@
+# (*2) Pada Akhiran agar menduplikasi
+
+#contoh 1
+print("    *    "*2)
+print("   * *   "*2)
+print("  *   *  "*2)
+print(" *     * "*2)
+print("***   ***"*2)
+print("  *   *  "*2)
+print("  *   *  "*2)
+print("  *****  "*2)
+
+#contoh 2
+print("         *\n       * *\n     *   *\n   *     *\n * * * * *\n"*2)
+
+#contoh 3
+print(" + * * * * +\n *         *\n *         *\n *         *\n + * * * * +\n"*2)
