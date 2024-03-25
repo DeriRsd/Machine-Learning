4015e26b0868a3550f3d66b9437097dfd5c342b9
DeriRsd <113176962+DeriRsd@users.noreply.github.com>
2024-03-25 20:47:44 +0700

    Update LatihanType.py

--- Minggu2-Part2/LatihanType.py
+++ Minggu2-Part2/LatihanType.py
@@ -1,5 +1,19 @@
+#Integer
+round = 3
+print(round)
+print(type(round))
+
+#Float
 height = 4.0
+print(height)
 print(type(height))
 
+#String
 nama = "Deri"
+print(nama)
 print(type(nama))
+
+#List or Array
+tall = [2, 1.79, 1.82, 1.90, 1.80]
+print(tall)
+print(type(tall))
